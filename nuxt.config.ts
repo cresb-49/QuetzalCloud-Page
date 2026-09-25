30f319da6b7e4e30be2cfa874959ed0d893e46d0
Benjamin <carlosbpac@gmail.com>
2026-09-24 19:44:45 -0600

    feat: versel analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
-  modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
+  modules: ['@nuxt/content', '@nuxtjs/tailwindcss','@vercel/analytics'],
   components: [{ path: '~/components', pathPrefix: false }],
   css: ['~/assets/css/tailwind.css', '~/assets/css/main.css'],
   routeRules: {
